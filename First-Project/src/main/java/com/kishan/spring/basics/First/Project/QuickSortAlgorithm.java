12348197792138989cf2eba06424a8094f2188d2
Kishan Jaiswal <kjaiswal8802@gmail.com>
2020-08-29 22:37:10 +0530

    added @Primary to give importance when multiple beans of same thing found

--- First-Project/src/main/java/com/kishan/spring/basics/First/Project/QuickSortAlgorithm.java
+++ First-Project/src/main/java/com/kishan/spring/basics/First/Project/QuickSortAlgorithm.java
@@ -1,5 +1,10 @@
 package com.kishan.spring.basics.First.Project;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary // to given importance to Quicksort
 public class QuickSortAlgorithm implements SortAlgorithm{
    
 	public int[] sort(int[] numbers)
